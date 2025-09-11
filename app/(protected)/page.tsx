703f503f30e03af25ee6b59c0dfe4a79132df8ee
Alison Barbosa <alisonnbarbosaaa@gmail.com>
2025-09-11 18:28:53 -0300

    feat: implementa a pagina dashboard

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,14 +1,38 @@
-// type UserData = {
-//   id: string;
-//   email: string;
-//   name: string;
-//   role?: string;
-// };
+import { MovementsTable } from "@/components/MovementsTable";
 
 export default async function Dashboard() {
   return (
-    <div>
-      <h1>Estoque app</h1>
-    </div>
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <input
+          type="text"
+          placeholder="Buscar..."
+          className="px-4 py-2 max-w-40 rounded-md shadow"
+        />
+        <div className="w-10 h-10 rounded-full bg-gray-300"></div>
+      </header>
+      <section className="space-y-4">
+        <h2 className="text-2xl font-bold">Dashboard</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="bg-white rounded-md p-4 shadow">
+            Produtos Cadastrados: <span className="font-bold">120</span>
+          </div>
+          <div className="bg-white rounded-md p-4 shadow">
+            Produtos em Falta: <span className="font-bold">5</span>
+          </div>
+          <div className="bg-white rounded-md p-4 shadow">
+            Movimentações Hoje: <span className="font-bold">18</span>
+          </div>
+        </div>
+
+        <div className="bg-white rounded-md p-4 shadow space-y-4">
+          <h3 className="font-semibold">Últimas Movimentações</h3>
+          <MovementsTable />
+        </div>
+        <div className="bg-white rounded-xl shadow p-8 text-center text-gray-500">
+          [ Gráfico Entradas vs Saídas ]
+        </div>
+      </section>
+    </>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export const Sidebar = () => {
   const { isAdmin } = useAuth();
   return (
-    <aside className="w-64 bg-white shadow-lg p-4 hidden md:block">
+    <aside className="w-72 bg-white shadow-lg p-4 hidden md:block">
       <h1 className="text-2xl font-bold mb-8">EstoqueApp</h1>
       <nav className="space-y-4">
         <Link href="/" className="block text-gray-700 hover:text-blue-600">
