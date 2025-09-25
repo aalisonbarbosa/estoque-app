0b848073a0e06f0edd5da8b39a308244ea56ae65
Alison Barbosa <alisonnbarbosaaa@gmail.com>
2025-09-25 10:17:00 -0300

    adiciona popup de feedback

---
+++ components/ui/Popup.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  isPopupVisible: boolean;
+  type: "sucess" | "error";
+  message: string;
+}
+
+export const Popup = ({ isPopupVisible, type, message }: Props) => {
+  return (
+    <div
+      className={`${
+        type === "sucess" ? "bg-green-400 w-40" : "bg-red-500"
+      } p-2 text-center rounded-md absolute left-1/2 bottom-0 transform -translate-x-1/2 transition-opacity duration-500 ${
+        isPopupVisible ? "opacity-100" : "opacity-0"
+      }`}
+    >
+      {message}
+    </div>
+  );
+};
