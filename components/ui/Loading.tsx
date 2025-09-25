d81c5b35a5c798b7786c8021fb6c823f39fa1d24
Alison Barbosa <alisonnbarbosaaa@gmail.com>
2025-09-25 10:15:55 -0300

    refactor: organiza os components em pastas

---
+++ components/ui/Loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-12 h-12 border-blue-500 border-2 border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+};
